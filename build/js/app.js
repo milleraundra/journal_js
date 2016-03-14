ee0ea89c6cd04663152e3bee2610d1e81b65e26c
aundra miller and torrence stratton <miller.aundra@gmail.com>
2016-03-14 11:05:18 -0700

    add del and clean to gulp

--- build/js/app.js
+++ build/js/app.js
@@ -25,4 +25,16 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $("#comment").submit(function(event){
+    event.preventDefault();
+    console.log("here we go!");
+    var email = $("input#email").val();
+    var comment_body = $("input#comment_body").val();
+    console.log(comment_body);
+    $("#comment").hide();
+    $("#notification").prepend('<p>' + email + '</p><p>'+ comment_body + '</p>');
+  });
+});
+
 },{"./../js/journal.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
     $('.output').append("<li>" + output.wordCount() + "</li>");
   });
 });
+
+$(document).ready(function(){
+  $("#comment").submit(function(event){
+    event.preventDefault();
+    console.log("here we go!");
+    var email = $("input#email").val();
+    var comment_body = $("input#comment_body").val();
+    console.log(comment_body);
+    $("#comment").hide();
+    $("#notification").prepend('<p>' + email + '</p><p>'+ comment_body + '</p>');
+  });
+});
