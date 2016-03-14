fbf1d370666d0c195bbe2c5e5f4057845e5aba10
aundra miller and torrence stratton <miller.aundra@gmail.com>
2016-03-14 09:26:26 -0700

    add working prompt to project

--- js/cli.js
+++ js/cli.js
@@ -1,12 +1,17 @@
 var JournalEntry = require('./journal.js').JournalEntry;
+var prompt = require('prompt');
+prompt.start();
 
-var title = prompt('Enter a title for your journal entry');
-var date = prompt('Enter the date of your entry');
-var body = prompt('Write your journal entry');
+// var title = prompt('Enter a title for your journal entry');
+// var date = prompt('Enter the date of your entry');
+// var body = prompt('Write your journal entry');
 
-var entry = new JournalEntry(title, date, body);
-var result = input.wordCount();
-console.log(result);
+prompt.get(['title', 'date', 'body'], function(err, entry) {
+  var entry = new JournalEntry(entry.title, entry.date, entry.body);
+  var result = entry.wordCount();
+  console.log(result);
+});
+
 
 // var JournalEntry = require('./journal.js').JournalEntry;
 // var input = new JournalEntry('Cat needs', '03132016', 'Today I went to the store and bought food for my cat.');
