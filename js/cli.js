faa8832311ef80fe673556c1b408639cdee8a02c
aundra miller and torrence stratton <tlstratton@gmail.com>
2016-03-14 09:15:14 -0700

    install npm and add prompt package

--- js/cli.js
+++ js/cli.js
@@ -1,5 +1,14 @@
-var journalEntry = require('./journal.js').journalEntry;
+var JournalEntry = require('./journal.js').JournalEntry;
 
-var input = new journalEntry('Cat needs', '03132016', 'Today I went to the store and bought food for my cat.');
+var title = prompt('Enter a title for your journal entry');
+var date = prompt('Enter the date of your entry');
+var body = prompt('Write your journal entry');
+
+var entry = new JournalEntry(title, date, body);
 var result = input.wordCount();
 console.log(result);
+
+// var JournalEntry = require('./journal.js').JournalEntry;
+// var input = new JournalEntry('Cat needs', '03132016', 'Today I went to the store and bought food for my cat.');
+// var result = input.wordCount();
+// console.log(result);
--- js/journal.js
+++ js/journal.js
@@ -1,10 +1,10 @@
-exports.journalEntry = function(title, date, body) {
+exports.JournalEntry = function(title, date, body) {
   this.title = title;
   this.date = date;
   this.body = body;
 };
 
-exports.journalEntry.prototype.wordCount = function() {
+exports.JournalEntry.prototype.wordCount = function() {
   var string = this.body;
   var string_array = string.split(" ");
   return string_array.length;
